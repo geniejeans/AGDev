36bced9f0427957a4f28f26e67788e6972ba7f60
hongsheng322 <hongsheng322@gmail.com>
2017-12-14 13:44:43 +0800

    Change tnak top texture and bullet speed

--- AGDev_Assignment1/Base/Source/WeaponInfo/LaserBlaster.cpp
+++ AGDev_Assignment1/Base/Source/WeaponInfo/LaserBlaster.cpp
@@ -50,7 +50,7 @@ void CLaserBlaster::Discharge(Vector3 position, Vector3 target, CPlayerInfo* _so
 											_direction,
 											10.0f,
 											2.0f,
-											2000.0f,
+											600.0f,
 											_source);
 			aLaser->SetIsLaser(true);
 			aLaser->SetCollider(false);
