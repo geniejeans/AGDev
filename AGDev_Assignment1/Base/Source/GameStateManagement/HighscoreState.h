238a2763c1c8cbb502fdb18519fd7b5cd4637158
hongsheng322 <hongsheng322@gmail.com>
2018-02-07 19:19:11 +0800

    added proj filter

--- AGDev_Assignment1/Base/Source/GameStateManagement/HighscoreState.h
+++ AGDev_Assignment1/Base/Source/GameStateManagement/HighscoreState.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "Scene.h"
+#include "Mtx44.h"
+#include "Mesh.h"
+#include "MatrixStack.h"
+#include "../FPSCamera.h"
+#include "../SpriteEntity.h"
+
+class SceneManager;
+//class Text ENtity
+class CHighScoreState :public Scene
+{
+public:
+	CHighScoreState();
+	~CHighScoreState();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+private:
+	FPSCamera camera;
+	SpriteEntity* IntroStateBackground;
+	// TextEntity* textObj[3];
+};
--- AGDev_Assignment1/Base/Source/GameStateManagement/ShopState.h
+++ AGDev_Assignment1/Base/Source/GameStateManagement/ShopState.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include "Scene.h"
+#include "Mtx44.h"
+#include "Mesh.h"
+#include "MatrixStack.h"
+#include "../FPSCamera.h"
+#include "../SpriteEntity.h"
+
+class SceneManager;
+//class Text ENtity
+class CShopState :public Scene
+{
+public:
+	CShopState();
+	~CShopState();
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+private:
+	FPSCamera camera;
+	SpriteEntity* IntroStateBackground;
+	// TextEntity* textObj[3];
+};
